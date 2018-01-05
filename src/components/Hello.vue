d9fe53310c2745b02125f2433e15cbe23b66a04b
numajiri <numajiri@numa.local>
2018-01-05 09:49:22 +0900

    first commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2><i class="fa fa-free-code-camp" aria-hidden="true"></i></h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  computed: {
+    msg() {
+      return this.$store.state.msg;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
